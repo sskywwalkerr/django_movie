f12291f85a231a860d4b4cc81f8bcd8084122a64
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-06-05 16:46:26 +0500

    OAUTH2

--- djangoProject_movie/settings.py
+++ djangoProject_movie/settings.py
@@ -42,10 +42,14 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'ckeditor',
     'ckeditor_uploader',
-    'django_filters',
     'djoser',
-    'drf_yasg',
 
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
+
+    'drf_yasg',
+    'django_filters',
 
     'movies',
 ]
@@ -222,13 +226,25 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+SOCIAL_AUTH_VK_OAUTH2_KEY = '5027406'
+SOCIAL_AUTH_VK_OAUTH2_SECRET = 'T9rjcM84R6lUi6jNlLA1'
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
 
@@ -277,14 +293,21 @@ DJOSER = {
 }
 
 SWAGGER_SETTINGS = {
-    'DEFAULT_FIELD_INSPECTORS': [
-        'drf_yasg.inspectors.CamelCaseJSONFilter',
-        'drf_yasg.inspectors.InlineSerializerInspector',
-        'drf_yasg.inspectors.RelatedFieldInspector',
-        'drf_yasg.inspectors.ChoiceFieldInspector',
-        'drf_yasg.inspectors.FileFieldInspector',
-        'drf_yasg.inspectors.DictFieldInspector',
-        'drf_yasg.inspectors.SimpleFieldInspector',
-        'drf_yasg.inspectors.StringDefaultFieldInspector',
-    ],
+   'USE_SESSION_AUTH': False,
+   'SECURITY_DEFINITIONS': {
+      'Your App API - Swagger': {
+         'type': 'oauth2',
+         'authorizationUrl': '/yourapp/o/authorize' ,
+         'tokenUrl': '/yourapp/o/token/',
+         'flow': 'accessCode',
+         'scopes': {
+          'read: groups': 'read groups',
+         }
+      }
+   },
+   'OAUTH2_CONFIG': {
+      'ClientID': 'yourAppClientId',
+      'clientSecret': 'yourAppClientSecret',
+      'AppName': 'имя вашего приложения'
+   },
 }
--- djangoProject_movie/urls.py
+++ djangoProject_movie/urls.py
@@ -6,10 +6,12 @@ from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('rest_framework_social_oauth2.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
