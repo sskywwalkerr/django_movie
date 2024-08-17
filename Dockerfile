a79ba6cdf3c6ccd8cf0e13628e8cf338652c2a02
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-08-17 15:58:36 +0500

    Dockerfile, settings, envdev

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,34 @@
 FROM python:3.8
 
+WORKDIR /app
+
 ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
 
-WORKDIR /usr/src/dm_rest
+#RUN apt update && apt install postgresql-dev gcc python3-dev musl-dev
 
-COPY ./requirements.txt /usr/src/requirements.txt
-RUN pip install -r /usr/src/requirements.txt
 
-COPY . /usr/src/dm_rest
 
-EXPOSE 8000
-#CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+RUN pip install --upgrade pip
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+
+
+COPY . .
+
+
+
+
+#FROM python:3.8
+#
+#ENV PYTHONDONTWRITEBYTECODE 1
+#ENV PYTHONUNBUFFERED 1
+#
+#WORKDIR /usr/src/dm_rest
+#
+#COPY ./requirements.txt /usr/src/requirements.txt
+#RUN pip install -r /usr/src/requirements.txt
+#
+#COPY . /usr/src/dm_rest
+#
+#EXPOSE 8000
+##CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- djangoProject_movie/settings.py
+++ djangoProject_movie/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8_v4a4u25j(siko3#l7b6jdto4jrrw5mg@a3e9@ws!w59cr6(#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
 
 
 # Application definition
@@ -91,37 +91,42 @@ WSGI_APPLICATION = 'djangoProject_movie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'movie',
-        #'USER': 'postgres',
-        #'PASSWORD': '272371rad',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-    #}
-#}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'movie',
+#         'USER': 'postgres',
+#         'PASSWORD': '272371rad',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'dm_db',
+#         'PORT': 5432
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'dm_db',
-        'PORT': 5432
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-#DATABASES = {
-    #'default': {
-        #'ENGINE': os.environ.get('POSTGRES_ENGINE', 'django.db.backends.sqlite3'),
-        #'NAME': os.environ.get('POSTGRES_DB', os.path.join(BASE_DIR, 'db.sqlite3')),
-        #'USER': os.environ.get('POSTGRES_USER', 'user'),
-        #'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
-        #'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        #'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-    #}
-#}
-
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.qlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
