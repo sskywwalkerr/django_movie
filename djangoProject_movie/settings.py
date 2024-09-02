eca3bbfdb5e1814cbb1e57f3902579452c45a8f3
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-09-02 20:28:00 +0500

    Dockerfile, nginx

--- djangoProject_movie/settings.py
+++ djangoProject_movie/settings.py
@@ -172,9 +172,10 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
